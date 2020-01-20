ca9ab6edc01d6c165a0dfbf862cfd91004ec873f
shubs12 <jungharesj7@gmail.com>
2020-01-20 14:57:49 +0530

    login signup

--- Agventure/src/router/index.js
+++ Agventure/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '@/components/home'
+import login from '@/components/login'
+import signup from '@/components/signup'
 
 Vue.use(Router)
 
@@ -11,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: signup
     }
   ]
 })
